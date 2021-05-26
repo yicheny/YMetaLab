7ca11e365f7a124e1b4bb9b3bc487f5d681d2fdc
yanglvfeng <yanglvfeng@croot.com>
2021-05-26 15:07:44 +0800

    update 动画效果演示

--- mouse-menu/src/component/CircleMenu.tsx
+++ mouse-menu/src/component/CircleMenu.tsx
@@ -4,45 +4,45 @@ import {IPositionConfig} from "../base";
 import styles from "./CircleMenu.module.scss";
 import {useGetCirclePosition} from "../utils";
 
-interface IItemOption{
+interface IItemOption {
     className?: string,
     style?: React.CSSProperties | undefined,
     onClick?: React.MouseEventHandler<HTMLSpanElement> | undefined,
     children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
 }
 
-interface ICircleMenuProps{
+interface ICircleMenuProps {
     className?: string,
     style?: React.CSSProperties | undefined,
     positionOptions?: IPositionConfig,
     items: IItemOption[]
 }
 
-export default function CircleMenu(props:ICircleMenuProps){
-    const {boxWidth=300,boxHeight=300,itemWidth=60,itemHeight=60,itemSize} = props.positionOptions || {};
+export default function CircleMenu(props: ICircleMenuProps) {
+    const {boxWidth = 300, boxHeight = 300, itemWidth = 60, itemHeight = 60, itemSize} = props.positionOptions || {};
     const getCirclePosition = useGetCirclePosition({
         boxWidth,
         boxHeight,
-        itemHeight:itemSize||itemHeight,
-        itemWidth:itemSize||itemWidth
+        itemHeight: itemSize || itemHeight,
+        itemWidth: itemSize || itemWidth
     });
     const count = props.items.length;
-    return <div className={clsx(styles.circleMenu,props.className)}
-                style={Object.assign({width:boxWidth,height:boxHeight},props.style)}>
+    return <div className={clsx(styles.circleMenu, props.className)}
+                style={Object.assign({width: boxWidth, height: boxHeight}, props.style)}>
         {
-            props.items.map((x,i)=>{
-                const positionInfo = getCirclePosition(count,i);
+            props.items.map((x, i) => {
+                const positionInfo = getCirclePosition(count, i);
                 const baseStyle = {
-                    width:itemWidth,
-                    height:itemHeight,
-                    bottom:positionInfo.bottom.concat('px'),
-                    left:positionInfo.left.concat('px'),
+                    width: itemSize || itemWidth,
+                    height: itemSize || itemHeight,
+                    bottom: positionInfo.bottom.concat('px'),
+                    left: positionInfo.left.concat('px'),
                 };
 
                 return <div key={i}
-                            style={Object.assign(baseStyle,x.style)}
+                            style={Object.assign(baseStyle, x.style)}
                             onClick={x.onClick}
-                            className={clsx(styles.item,x.className)}>
+                            className={clsx(styles.item, x.className)}>
                     {x.children}
                 </div>
             })
