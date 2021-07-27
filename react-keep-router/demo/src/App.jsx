86640085d6b3b0fbbee1e0b32a370f5abe6cb331
yanglvfeng <yanglvfeng@croot.com>
2021-07-27 18:57:41 +0800

    update 修改View显示

--- react-keep-router/demo/src/App.jsx
+++ react-keep-router/demo/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Menu from "./Menu.jsx";
 
@@ -17,5 +17,15 @@ export default function App(){
 function View(props){
     return <div>
         <h2>标题：{props.title}</h2>
+        <Counter key={props.title}/>
     </div>
+}
+
+function Counter(){
+    const [count,setCount] = useState(0)
+
+    return <>
+        <h3>Count：{count}</h3>
+        <button onClick={()=>setCount(count+1)}>增加</button>
+    </>
 }
