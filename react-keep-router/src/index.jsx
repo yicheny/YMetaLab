580a583960d3073ed533189d8dfe69982ece7a38
yanglvfeng <yanglvfeng@croot.com>
2021-08-05 14:56:57 +0800

    update 导出useKeepEffect

--- react-keep-router/src/index.jsx
+++ react-keep-router/src/index.jsx
@@ -1,3 +1,4 @@
 // export {Test} from './Test.jsx';
-export {default as KeepAliveScope} from './component/KeepAlive/KeepAliveScope.jsx';
-export {default as KeepAlive} from './component/KeepAlive/KeepAlive.jsx';
+export { default as KeepAliveScope } from './component/KeepAlive/KeepAliveScope.jsx';
+export { default as KeepAlive } from './component/KeepAlive/KeepAlive.jsx';
+export { default as useKeepEffect } from './component/KeepAlive/useKeepEffect';
