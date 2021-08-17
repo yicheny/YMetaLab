b364a78b5c29f6d384560b0ca4d5392d681c13f6
YLF <1463020554@qq.com>
2021-08-17 08:39:14 +0800

    add 测试LazyView

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,6 +2,7 @@ import {Switch,Route} from "react-router-dom";
 import SuspenseView from "./View/SuspenseView";
 import CacheView from "./View/CacheView";
 import ErrorBoundaryView from "./View/ErrorBoundaryView";
+import LazyView from "./View/LazyView";
 
 function App() {
   return <div className="app">
@@ -9,6 +10,7 @@ function App() {
       <Route path='/suspense' component={SuspenseView}/>
       <Route path='/cache' component={CacheView}/>
       <Route path='/error-boundary' component={ErrorBoundaryView}/>
+      <Route path='/lazy' component={LazyView}/>
       <Route component={SuspenseView}/>
     </Switch>
   </div>
