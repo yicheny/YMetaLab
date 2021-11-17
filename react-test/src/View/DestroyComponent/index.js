109670fb970c11ece9658c0794717be77f52b8e7
yanglvfeng <yanglvfeng@croot.com>
2021-11-17 15:20:24 +0800

    update 模拟删除节点

--- react-test/src/View/DestroyComponent/index.js
+++ react-test/src/View/DestroyComponent/index.js
@@ -14,18 +14,28 @@ export class DestroyComponent extends PureComponent{
         this.store = [];
         this.nodes = { }
         this.timeId = null;
+        this.container = null;
     }
 
     componentDidMount(prevProps, prevState, snapshot) {
-        this.timeId = setInterval(this.update,300000)
+        this.timeId = setInterval(this.update,3000000)
     }
 
     componentWillUnmount() {
         if(this.timeId) clearInterval(this.timeId)
     }
 
+    setCurrentKey = () => {
+
+    }
+
     update = () => {
+        if(Object.values(this.nodes).length > 3) return ;
         console.log('update开始执行啦！',this.store, this.nodes)
+        console.log(Object.values(this.nodes))
+
+        let node;
+        let dKey;
         if(this.store.length < 2){
             key+=1;
             this.store.push({
@@ -34,13 +44,28 @@ export class DestroyComponent extends PureComponent{
             })
         }else{
             const x = this.store.pop();
+            dKey = x.key;
+            // 必须更新之后删除，否则删不掉这个键，只是键值变成null
+            // delete this.nodes[dKey];
         }
-        this.forceUpdate();
+        this.forceUpdate(()=>{
+            if(node) {
+                // node.remove();
+                delete this.nodes[dKey];
+
+                //通过container子元素我们会发现页面中的元素和我们卸载的node是不同元素
+                //其node实际上已经从DOM树里被卸载了
+                // console.log(node,this.container);
+                // console.dir(this.container);
+            }
+            // delete this.nodes[x.key];
+            // console.log(this.nodes)
+        });
     }
 
     render(){
         console.log(this.store)
-        return <div className="destroy-component">
+        return <div className="destroy-component" ref={container => this.container = container}>
             测试测试！
             {
                 this.store.map((x)=>{
