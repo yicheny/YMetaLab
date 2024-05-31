acf9e52e30b90f515249e19f2542d330fca0e831
yicheny <1463020554@qq.com>
2024-05-31 15:29:52 +0800

    如果设置 required:true 则标题字体为红色

--- exceljs-demo/src/downloadExcel/utils.js
+++ exceljs-demo/src/downloadExcel/utils.js
@@ -1,21 +1,33 @@
 // excelUtils.js
 
-// 生成 Excel 文件的方法
 async function generateExcelWorkbook(data, columns) {
     const workbook = new ExcelJS.Workbook();
     const worksheet = workbook.addWorksheet('Sheet1');
 
+    // 生成标题
     const headers = columns.map(col => col.header);
-    worksheet.addRow(headers);
+    const headerRow = worksheet.addRow(headers);
 
+    // 设置标题字体颜色
+    columns.forEach((col, index) => {
+        const cell = headerRow.getCell(index + 1);
+        if (col.required) {
+            cell.font = {
+                color: { argb: 'FFFF0000' } // 红色字体
+            };
+        }
+    });
+
+    // 填充数据
     data.forEach(item => {
         const rowData = columns.map(col => item[col.bind]);
         worksheet.addRow(rowData);
     });
 
+    // 添加标题的批注
     columns.forEach((col, index) => {
         const cell = worksheet.getCell(`${String.fromCharCode(65 + index)}1`);
-        if(col.comment) cell.note = col.comment;
+        if (col.comment) cell.note = col.comment;
     });
 
     return workbook;
